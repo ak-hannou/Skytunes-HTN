168e80070746bb69881b320f97a4e2bf2ff533f1
Akram Hannoufa <hannoufa@mcmaster.ca>
2021-09-18 15:15:56 -0400

    starter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { TextField, Button } from "@mui/material";
+import { Gradient } from "react-gradient";
+import Logo from "./logo.svg";
+
+const gradients = [
+  ["#F8D770", "#E27AB2"],
+  ["#574B95", "#2459B6", "#OO2966"],
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Gradient
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            height: "100%",
+            width: "100%",
+            alignItems: "center",
+          }}
+          gradients={gradients} // required
+          property="background"
+          duration={9000}
+          angle="0deg"
         >
-          Learn React
-        </a>
+          <div style={{ display: "flex", alignItems: 'center', justifyContent: "space-between" }}>
+            <img style={{height: 250, width: 250}} src={Logo} alt="skytunes logo" />
+            <h3 style={{marginLeft: 100, textAlign: 'right', color: '#OO2966'}}>music from your <br/> environment.</h3>
+          </div>
+
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <p style={{ marginTop: 0, textAlign: "left" }}>
+              connect your Spotify
+              <br />
+              and location to get started
+            </p>
+
+            <TextField required id="outlined-required" label="Required" />
+          </div>
+          
+        </Gradient>
       </header>
+      <footer>
+        
+        </footer>
     </div>
   );
 }
